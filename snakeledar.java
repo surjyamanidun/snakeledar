0bca450e556e05309f8a731436e242787179a543
surjya <surjyamaniduan@gmail.com>
2021-09-18 12:54:06 +0530

    rolldie

--- snakeledar.java
+++ snakeledar.java
@@ -1,5 +1,17 @@
 package com.snakeandleder;
+import java.util.Random;
 
 public class snakeledar {
 
-}
+	
+	
+	    public static void main(String[] args) {
+	        System.out.println("Welcome to Snake and Ladder Game");
+	        int startPosition = 0;
+	        Random random = new Random();
+	        int diceNumber = random.nextInt(6)+1;
+	        System.out.println("Dice Number : " + diceNumber);
+	    }
+	}
+
+
